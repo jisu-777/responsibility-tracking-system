fb82eec0e221b8d27bea17b542ab7bf81537f52e
jisu park <jisu.p.park@pwc.com>
2025-08-04 14:36:49 +0900

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import someConfig from "some-other-config-you-use";
+import eslintConfigPrettier from "eslint-config-prettier";
+import js from "@eslint/js";
+import globals from "globals";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +15,23 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      // 여기에 추가적인 규칙을 설정할 수 있습니다
+      "no-unused-vars": "warn",
+      "no-console": "warn"
+    }
+  }
 ];
 
-export default eslintConfig;
+export default [
+  someConfig,
+  eslintConfigPrettier,
+];
